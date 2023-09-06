b1b350217c2dd1dc7fafbde4366b1a5b21b07f5f
Francis Lagares <francis.lagares@gmail.com>
2023-09-06 19:42:01 +0200

    feat: project scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className='h-screen text-3xl font-bold'>
+      <h1>Vite + React + TypeScript</h1>
+
+      <p>A React Starter Template to build amazing apps.</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,27 @@
+import arrowRight from './arrow-right.svg';
+import chevronLeft from './chevron-left.svg';
+import cheveronRight from './chevron-right.svg';
+import copyrightSign from './copyright-sign.svg';
+import facebook from './facebook.svg';
+import hamburger from './hamburger.svg';
+import instagram from './instagram.svg';
+import shieldTick from './shield-tick.svg';
+import star from './star.svg';
+import support from './support.svg';
+import truckFast from './truck-fast.svg';
+import twitter from './twitter.svg';
+
+export {
+  arrowRight,
+  cheveronRight,
+  chevronLeft,
+  copyrightSign,
+  facebook,
+  hamburger,
+  instagram,
+  shieldTick,
+  star,
+  support,
+  truckFast,
+  twitter,
+};
