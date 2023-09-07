f5a084f93405e2408d8c093513e60afa5b5dc0a0
Francis Lagares <francis.lagares@gmail.com>
2023-09-07 20:43:05 +0200

    feat: create section structure components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import { describe, test } from 'vitest';
 
 import App from './App';
 
 describe('App', () => {
   test('renders without crashing', () => {
     render(<App />);
-
-    const heading = screen.getByRole('heading');
-
-    expect(heading).toBeVisible();
-    expect(heading).toHaveTextContent('Vite + React');
 
     // screen.debug();
   });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import {
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from '@/sections';
+
 const App = () => {
   return (
-    <div className='h-screen text-3xl font-bold'>
-      <h1>Vite + React + TypeScript</h1>
-
-      <p>A React Starter Template to build amazing apps.</p>
-    </div>
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-l wide:padding-r padding-b'>Hero</section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='padding bg-pale-blue'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x w-full py-16 sm:py-32'>
+        <Subscribe />
+      </section>
+      <section className='padding-x padding-t bg-black pb-8'>
+        <Footer />
+      </section>
+    </main>
   );
 };
 
